052bc9bf95161f7b9e8b3c634b6a1d818c77ee70
JeffAntony <142667486+JeffAntony@users.noreply.github.com>
2023-09-17 16:21:49 +0530

    Vector.java

---
+++ Vector.java
@@ -0,0 +1,291 @@
+
+
+//import java.util.*;
+//public class Vector {
+//    public static void main(String[] args) {
+//        Vector k = new Vector ();
+//        k.addElement("ravi");
+//        k.add("vijay");
+//        k.add("ramesh");
+//        k.add("gopal");
+//        k.add("ganesh");
+//        System.out.println("Vector before addiing" + k);
+//    }
+//}
+
+//____________________________________________________________________________________________________________________
+
+import java.util.*;
+
+//public class Vector {
+//    //    public static void main(String[] args) {
+//Vector<String> n = new Vector<String>();
+//        n.add("radha");
+//        n.add("krishna");
+//        n.add("raavan");
+//        n.add("bheem");
+//        n.add("tina");
+//
+//      for(String s: n)
+//              {
+//        System.out.println("" + s);
+//              }
+
+// for(int i=0; i<n.size(); i++)
+//{
+//    System.out.print(get(i))
+//        }
+
+//____________________________________________________________________________________________________________________________
+
+//
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args) {
+//        Vector<Integer> n = new Vector<Integer>();
+//        n.add(123);
+//        n.add(34);
+//        n.add(25);
+//        n.add(243);
+//        n.add(12);
+//
+//        for(int i=0; i<n.size(); i++)
+//        {
+//
+//            System.out.println(n.get(i));
+//        }
+//    }
+//}
+
+//_________________________________________________________________________________________________________________________
+
+//
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args) {
+//        Vector<Integer> n = new Vector<Integer>();
+//        n.add(12);
+//        n.add(37);
+//        n.add(23);
+//        n.add(43);
+//        n.add(121);
+//
+//        for(int i=0; i<n.size(); i++)
+//        {
+//
+//            System.out.println(n.get(i));
+//        }
+//    }
+//}
+
+//________________________________________________________________________________________________________________________
+
+
+import java.util.*;
+
+//public class Vector {
+//    //    public static void main(String[] args) {
+//Vector<Integer> n = new Vector<Integer>();
+//        n.add(123);
+//        n.add(34);
+//        n.add(32);
+//        n.add(193);
+//        n.add(267);
+//
+//      for(Integer s: n)
+//              {
+//        System.out.println("" + s);
+//              }
+
+// for(int i=0; i<n.size(); i++)
+//{
+//    System.out.print(n.get(i))
+//        }
+
+//_________________________________________________________________________________________________________________________
+//
+// Inserting elements in Wraper classes
+import java.util.*;
+
+
+//(1)
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args)
+//    {
+//        Vector<Integer> b= new Vector<Integer>();
+//        b.add(23);
+//        b.add(34);
+//        b.add(56);
+//        b.add(76);
+//        System.out.println(b);
+//    }
+//}
+
+
+
+
+//(2)
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args)
+//    {
+//        Vector<Character> b= new Vector<Character>();
+//        b.add('f');
+//        b.add('h');
+//        b.add('h');
+//        b.add('d');
+//        System.out.println(b);
+//    }
+//}
+
+//(3)
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args)
+//    {
+//        Vector<Float> b= new Vector<Float>();
+//        b.add(1.2f);
+//        b.add(4.2f);
+//        b.add(4.5f);
+//        b.add(3.2f);
+//        System.out.println(b);
+//    }
+//}
+
+
+//(4)
+//import java.util.*;
+//
+//public class B {
+//    public static void main(String[] args)
+//    {
+//       Vector<Double> b= new Vector<Double>();
+//        b.add(1.267);
+//        b.add(4.245);
+//        b.add(4.545);
+//        b.add(3.245);
+//        System.out.println(b);
+//    }
+//}
+
+
+
+//(5)
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args)
+//    {
+//       Vector<String> l= new Vector<String>();
+//        l.add("1267");
+//        l.add("Sting");
+//        l.add("fgh");
+//        l.add("gir");
+//        System.out.println(l);
+//    }
+//}
+
+
+//(6)
+import java.util.*;
+
+//public class Vector {
+//    public static void main(String[] args)
+//    {
+//        ArrayList<Long> l= new ArrayList<Long>();
+//        l.add((long)5465);
+//        l.add((long)546);
+//        l.add((long)463);
+//        l.add((long)3454);
+//        System.out.println(l);
+//    }
+//}
+
+//(7)
+
+//
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args)
+//    {
+//        Vector <Short> l= new Vector<Short>();
+//       l.add((short)23);
+//        l.add((short)10);
+//        l.add((short)12);
+//        l.add((short)232);
+//        System.out.println(l);
+//    }
+//}
+
+
+//(8)
+//
+//import java.util.*;
+//
+//public class Vector {
+//    public static void main(String[] args)
+//    {
+//        Vector<Boolean> l= new Vector<Boolean>();
+//        l.add(true);
+//        l.add(false);
+//        l.add(true);
+//        l.add(false);
+//          l.add(true);
+//        System.out.println(l);
+//    }
+//}
+
+//____________________________________________________________________________________________________________
+
+// Finding odd and even numbers from vector
+
+//import java.util.LinkedList;
+
+//public class Vector {
+//    public static void main(String[] args) {
+//
+//        Vector<Integer> k = new Vector<Integer>();
+//        k.add(4);
+//        k.add(89);
+//        k.add(13);
+//        k.add(12);
+//        k.add(18);
+//        k.add(24);
+//        k.add(13);
+//        int c=0;
+//        for(int i=0; i<k.size(); i++)
+//        {
+//            if(k.get(i)%2==0)
+//            {
+//                System.out.println("even numbers are = "+ k.get(i));
+//            }
+//            else
+//            {
+//                System.out.println("odd numbers are ="+k.get(i));
+//            }
+//        }
+//
+//    }
+//}
+
+
+//import java.util.LinkedList;
+
+//public class Vector {
+//    public static void main(String[] args) {
+//
+//        Vector<Integer> k = new Vector<Integer>();
+//        k.add(4);
+//        k.add(89);
+//        k.add(13);
+//        k.add(12);
+//        k.add(18);
+//        k.add(24);
+//        k.add(13);
