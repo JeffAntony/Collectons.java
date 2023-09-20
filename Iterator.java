d5bb1c90b439b1999306292b0af3001aac1ca42a
JeffAntony <142667486+JeffAntony@users.noreply.github.com>
2023-09-20 20:07:30 +0530

    Update Iterator.java

--- Iterator.java
+++ Iterator.java
@@ -439,3 +439,795 @@
 //        }
 //    }
 //}
+
+//Assignment
+
+//(1)
+//* for printing String elements * in ArrayList
+
+
+//import java.util.*;
+//
+//public class Iterator {
+//    public static void main(String[] args) {
+//
+//        ArrayList <String> k = new ArrayList<>();
+//
+//        k.add("king");
+//        k.add("Queen");
+//        k.add("Pawn");
+//        k.add("Knight");
+//        int s=0;
+//
+//        while(s<k.size())
+//        {
+//            String l= k.get(s);
+////            System.out.println(l);
+//            s++;
+//        }
+//
+//        for(int i=0; i<k.size(); i++)
+//        {
+////            System.out.println("the elements are = "+k.get(i));
+//        }
+//
+//        for(String t: k)
+//        {
+//            System.out.println(t);
+//        }
+////        using iterator
+//       Iterator itr = k.iterator();
+//        while(itr.hasNext())
+//        {
+//
+//            System.out.println(itr.next()+" ");
+//        }
+//    }
+//
+//}
+
+//* for printing Integer elements */ in ArrayList
+
+//
+//import java.util.*;
+//
+//public class Iterator {
+//    public static void main(String[] args) {
+//
+//        ArrayList <String> k = new ArrayList<>();
+//
+//        k.add("king");
+//        k.add("Queen");
+//        k.add("Pawn");
+//        k.add("Knight");
+//        int s=0;
+//
+//        while(s<k.size())
+//        {
+//            String l= k.get(s);
+////            System.out.println(l);
+//            s++;
+//        }
+//
+//        for(int i=0; i<k.size(); i++)
+//        {
+////            System.out.println("the elements are = "+k.get(i));
+//        }
+//
+//        for(String t: k)
+//        {
+//            System.out.println(t);
+//        }
+////        using iterator
+//        Iterator itr = k.iterator();
+//        while(itr.hasNext())
+//        {
+//
+//            System.out.println(itr.next()+" ");
+//        }
+//    }
+//
+//}
+//_________________________________________________________________________________________________________________________
+
+//(2)
+
+// printing in Linkedlist for String
+//
+//
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        LinkedList <Integer> k = new LinkedList<>();
+//
+//        k.add(234);
+//        k.add(365);
+//        k.add(112);
+//        k.add(668);
+//        int s=0;
+//
+//        while(s<k.size())
+//        {
+//            Integer l= k.get(s);
+////            System.out.println(l);
+//            s++;
+//        }
+//
+//        for(int i=0; i<k.size(); i++)
+//        {
+////            System.out.println("the elements are = "+k.get(i));
+//        }
+//
+//        for(Integer t: k)
+//        {
+//            System.out.println(t);
+//        }
+////        using iterator
+//        Iterator itr = k.iterator();
+//        while(itr.hasNext())
+//        {
+//
+//            System.out.println(itr.next()+" ");
+//        }
+//    }
+//
+//}
+
+//printing in Linkedlist for Integers
+
+
+//import java.util.*;
+//
+//import java.util.*;
+//
+//public class Iterator {
+//    public static void main(String[] args) {
+//
+//        LinkedList<String> k = new LinkedList<>();
+//
+//        k.add("king");
+//        k.add("Queen");
+//        k.add("Pawn");
+//        k.add("Knight");
+//        int s = 0;
+//
+//        while (s < k.size()) {
+//            String l = k.get(s);
+////            System.out.println(l);
+//            s++;
+//        }
+//
+//        for (int i = 0; i < k.size(); i++) {
+////            System.out.println("the elements are = "+k.get(i));
+//        }
+//
+//        for (String t : k) {
+//            // System.out.println(t);
+//        }
+////        using iterator
+//        Iterator l = k.iterator();
+//        while (l.hasNext()) {
+//            System.out.println(l.next());
+//        }
+//
+//
+//    }
+//}
+
+
+// (3)
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Vector<String> k = new Vector<>();
+
+//         k.addElement("king");
+//         k.addElement("Queen");
+//         k.addElement("Pawn");
+//         k.addElement("Knight");
+//         int s = 0;
+
+//         while (s < k.size()) {
+//             String l = k.get(s);
+// //            System.out.println(l);
+//             s++;
+//         }
+
+//         for (int i = 0; i < k.size(); i++) {
+// //            System.out.println("the elements are = "+k.get(i));
+//         }
+
+//         for (String t : k) {
+//             // System.out.println(t);
+//         }
+// //        using iterator
+//       Iterator l= k.iterator();
+//       while(l.hasNext())
+//       {
+//           System.out.println(l.next());
+//       }
+//         }
+
+
+
+//     }
+
+//  for adding Integer elements
+
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Vector<Integer> k = new Vector<>();
+
+//         k.addElement(23);
+//         k.addElement(24);
+//         k.addElement(67);
+//         k.addElement(29);
+//         int s = 0;
+
+//         while (s < k.size()) {
+//             int l = k.get(s);
+//             System.out.println(l);
+//             s++;
+//         }
+
+//         for (int i = 0; i < k.size(); i++) {
+// //            System.out.println("the elements are = "+k.get(i));
+//         }
+
+//         for (int t : k) {
+//             // System.out.println(t);
+//         }
+// //        using iterator
+//       Iterator l= k.iterator();
+//       while(l.hasNext())
+//       {
+//         //   System.out.println(l.next());
+//       }
+//         }
+
+
+
+//     }
+
+// ____________________________________________________________________________________________________________________________
+
+
+// (3)
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Vector<String> k = new Vector<>();
+
+//         k.addElement("king");
+//         k.addElement("Queen");
+//         k.addElement("Pawn");
+//         k.addElement("Knight");
+//         int s = 0;
+
+//         while (s < k.size())
+//         {
+//             String l = k.get(s);
+// //            System.out.println(l);
+//             s++;
+//         }
+
+//         for (int i = 0; i < k.size(); i++) {
+// //            System.out.println("the elements are = "+k.get(i));
+//         }
+
+//         for (String t : k) {
+//             // System.out.println(t);
+//         }
+// //        using iterator
+//       Iterator l= k.iterator();
+//       while(l.hasNext())
+//       {
+//           System.out.println(l.next());
+//       }
+//         }
+
+
+
+//     }
+
+//  for adding Integer elements
+
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Vector<Integer> k = new Vector<>();
+
+//         k.addElement(23);
+//         k.addElement(24);
+//         k.addElement(67);
+//         k.addElement(29);
+//         int s = 0;
+
+//         while (s < k.size()) {
+//             int l = k.get(s);
+//             System.out.println(l);
+//             s++;
+//         }
+
+//         for (int i = 0; i < k.size(); i++) {
+// //            System.out.println("the elements are = "+k.get(i));
+//         }
+
+//         for (int t : k) {
+//             // System.out.println(t);
+//         }
+// //        using iterator
+//       Iterator l= k.iterator();
+//       while(l.hasNext())
+//       {
+//         //   System.out.println(l.next());
+//       }
+//         }
+
+
+
+//     }
+
+// ____________________________________________________________________________________________________________________________
+
+
+
+// (3)
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Stack<String> k = new Vector<>();
+
+//         k.addElement("king");
+//         k.addElement("Queen");
+//         k.addElement("Pawn");
+//         k.addElement("Knight");
+//         int s = 0;
+
+//         while (s < k.size()) {
+//             String l = k.get(s);
+// //            System.out.println(l);
+//             s++;
+//         }
+
+//         for (int i = 0; i < k.size(); i++) {
+// //            System.out.println("the elements are = "+k.get(i));
+//         }
+
+//         for (String t : k) {
+//             // System.out.println(t);
+//         }
+// //        using iterator
+//       Iterator l= k.iterator();
+//       while(l.hasNext())
+//       {
+//           System.out.println(l.next());
+//       }
+//         }
+
+
+
+//     }
+//____________________________________________________________________________________________________________________________
+
+
+//  for adding Integer elements
+//
+// import java.util.*;
+//   import java.util.Iterator;
+//
+// public class Iterator {
+//     public static void main(String[] args) {
+//
+//         Stack<Integer> k = new Stack<>();
+//
+//         k.addElement(23);
+//         k.addElement(24);
+//         k.addElement(67);
+//         k.addElement(29);
+//         int s = 0;
+//
+//         while (s < k.size()) {
+//             int l = k.get(s);
+//             System.out.println(l);
+//             s++;
+//         }
+//
+//         for (int i = 0; i < k.size(); i++) {
+// //            System.out.println("the elements are = "+k.get(i));
+//         }
+//
+//         for (int t : k) {
+//             // System.out.println(t);
+//         }
+// //        using iterator
+//       Iterator l= k.iterator();
+//       while(l.hasNext())
+//       {
+//            System.out.println(l.next());
+//       }
+//         }
+//
+//
+//
+//     }
+
+// ____________________________________________________________________________________________________________________________
+
+//(3)
+// adding and removing elements in the while loop
+
+// (a)
+
+// import java.util.*;
+//
+// public class Iter {
+//     public static void main(String[] args) {
+//
+//         ArrayList<Integer> al = new ArrayList<>();
+//
+//         al.add(12);
+//         al.add(334);
+//         al.add(23);
+//         al.add(45);
+//
+//
+////
+//
+//
+//         Iterator<Integer> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             int r = it.next();
+//             System.out.println(r);
+//
+//             if(r%2==0)
+//             {
+//                 it.remove();
+//             }
+//
+//         }
+//
+//
+////         al.addAll(n);
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+
+// import java.util.*;
+//
+// public class Iter {
+//     public static void main(String[] args) {
+//
+//         ArrayList<String> al = new ArrayList<>();
+//
+//         al.add("hen");
+//         al.add("Goat");
+//         al.add("Cow");
+//         al.add("Buffalo");
+//
+//
+//         ArrayList<String> n = new ArrayList<>();
+//
+//
+//         Iterator<String> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             String r = it.next();
+//             System.out.println(r);
+//             n.add("Meat");
+//             n.add("Egg");
+//         }
+//
+//
+//         al.addAll(n);
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+
+// import java.util.*;
+//
+// public class Iter {
+//     public static void main(String[] args) {
+//
+//         ArrayList<Integer> al = new ArrayList<>();
+//
+//         al.add(12);
+//         al.add(334);
+//         al.add(23);
+//         al.add(45);
+//
+//
+//
+//
+//
+//         Iterator<Integer> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             int r = it.next();
+//             System.out.println(r);
+//
+//
+//
+//                      {
+//                          it.remove();
+//                      }
+//         }
+//
+//
+//
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+
+ import java.util.*;
+
+// public class Iter {
+//     public static void main(String[] args) {
+//
+//         ArrayList<String> al = new ArrayList<>();
+//
+//         al.add("hen");
+//         al.add("Goat");
+//         al.add("Cow");
+//         al.add("Buffalo");
+//
+//
+//         ArrayList<String> n = new ArrayList<>();
+//
+//
+//         Iterator<String> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             String r = it.next();
+//             System.out.println(r);
+//             n.add("Camel");
+//             n.add("Horse");
+//
+//
+//         }
+//
+//         al.addAll(n);
+//
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+// ____________________________________________________________________________________
+
+
+// (b)
+
+// import java.util.*;
+//
+// public class Iter {
+//     public static void main(String[] args) {
+//
+//        LinkedList <Integer> al = new LinkedList<>();
+//
+//         al.add(12);
+//         al.add(334);
+//         al.add(23);
+//         al.add(45);
+//
+//
+//        LinkedList<Integer> n = new LinkedList<>();
+//
+//
+//         Iterator<Integer> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             int r = it.next();
+//             System.out.println(r);
+//             n.add(65);
+//             n.add(39);
+//         }
+//
+//
+//         al.addAll(n);
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+
+// import java.util.*;
+//
+// public class Iter {
+//     public static void main(String[] args) {
+//
+//         LinkedList<String> al = new LinkedList<>();
+//
+//         al.add("hen");
+//         al.add("Goat");
+//         al.add("Cow");
+//         al.add("Buffalo");
+//
+//
+//         LinkedList<String> n = new LinkedList<>();
+//
+//
+//         Iterator<String> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             String r = it.next();
+//             System.out.println(r);
+//             al.remove()
+//         }
+//
+//
+//
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+// ____________________________________________________________________________________
+
+// (c)
+
+// import java.util.*;
+//
+// public class Iter{
+//     public static void main(String[] args) {
+//
+//        Vector <Integer> al = new Vector<>();
+//
+//         al.add(12);
+//         al.add(334);
+//         al.add(23);
+//         al.add(45);
+//
+//
+//           Vector<Integer> n = new    Vector<>();
+//
+//
+//         Iterator<Integer> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             int r = it.next();
+//             System.out.println(r);
+//             n.add(65);
+//             n.add(39);
+//         }
+//
+//
+//         al.addAll(n);
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+
+// import java.util.*;
+//
+// public class Iter {
+//     public static void main(String[] args) {
+//
+//           Vector<String> al = new Vector<>();
+//
+//         al.add("hen");
+//         al.add("Goat");
+//         al.add("Cow");
+//         al.add("Buffalo");
+//
+//
+//
+//
+//         Iterator<String> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             String r = it.next();
+//             System.out.println(r);
+//             it.remove();
+//         }
+//
+//
+//
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+// ____________________________________________________________________________________
+
+
+// (d)
+// import java.util.*;
+//
+// public class Iterator {
+//     public static void main(String[] args) {
+//
+//        Stack <Integer> al = new Stack<>();
+//
+//         al.add(12);
+//         al.add(334);
+//         al.add(23);
+//         al.add(45);
+//
+//
+//           Stack<Integer> n = new   Stack<>();
+//
+//
+//         Iterator<Integer> it = al.iterator();
+//
+//         while (it.hasNext())
+//         {
+//             int r = it.next();
+//             System.out.println(r);
+//             it.remove();
+//
+//         }
+//
+//
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+//
+
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//           Stack<String> al = new Stack<>();
+
+//         al.add("hen");
+//         al.add("Goat");
+//         al.add("Cow");
+//         al.add("Buffalo");
+
+
+//        Stack<String> n = new Stack<>();
+
+
+//         Iterator<String> it = al.iterator();
+
+//         while (it.hasNext())
+//         {
+//             int r = it.next();
+//             System.out.println(r);
+//             n.add("Meat");
+//             n.add("Egg");
+//         }
+
+
+//         al.addAll(n);
+//         System.out.println("After adding the new elements ");
+//         System.out.println(al);
+//     }
+// }
+
+
+
+
+
