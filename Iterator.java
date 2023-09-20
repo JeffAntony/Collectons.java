b12e05c2ae8396b9bd07788841982b37f344f1f6
JeffAntony <142667486+JeffAntony@users.noreply.github.com>
2023-09-20 20:06:01 +0530

    Create Iterator.java

---
+++ Iterator.java
@@ -0,0 +1,441 @@
+
+//Even and odd numbers through Iterator
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        ArrayList<Integer> al = new ArrayList<>();
+//        al.add(34);
+//        al.add(57);
+//       Iterator <Integer> k= al.iterator();
+//
+//       while(k.hasNext())
+//       {
+//           int i=k.next();
+//           if(i%2==0)
+//           {
+//               System.out.println("Even numbers "+i);
+//            }
+//           else
+//           {
+//               System.out.println("Odd numbers "+i);
+//           }
+//
+//        }
+//    }
+//}
+//
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        LinkedList<Integer> al = new LinkedList<>();
+//        al.add(34);
+//        al.add(57);
+//        Iterator <Integer> k= al.iterator();
+//
+//        while(k.hasNext())
+//        {
+//            int i=k.next();
+//            if(i%2==0)
+//            {
+//                System.out.println("Even numbers "+i);
+//            }
+//            else
+//            {
+//                System.out.println("Odd numbers "+i);
+//            }
+//
+//        }
+//    }
+//}
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        Vector<Integer> al = new Vector<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        Iterator <Integer> k= al.iterator();
+//
+//        while(k.hasNext())
+//        {
+//            int i=k.next();
+//            if(i%2==0)
+//            {
+//                System.out.println("Even numbers "+i);
+//            }
+//            else
+//            {
+//                System.out.println("Odd numbers "+i);
+//            }
+//
+//        }
+//    }
+//}
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        Stack<Integer> al = new Stack<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        Iterator <Integer> k= al.iterator();
+//
+//        while(k.hasNext())
+//        {
+//            int i=k.next();
+//            if(i%2==0)
+//            {
+//                System.out.println("Even numbers "+i);
+//            }
+//            else
+//            {
+//                System.out.println("Odd numbers "+i);
+//            }
+//
+//        }
+//    }
+//}
+
+//_________________________________________________________________________________________________________________________
+
+//ListIterator in front direction.
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        List <Integer> al = new ArrayList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//    }
+//}
+
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        List <Integer> al = new LinkedList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//    }
+//}
+
+//___________________________________________________________________________________________________________________________
+
+//ListIterator in backward dircetion
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args)
+//    {
+//
+//        List <Integer> al = new LinkedList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//        System.out.println("After transversing");
+//        while(k.hasPrevious())
+//        {
+//            System.out.println(k.previous());
+//
+//        }
+//    }
+//}
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args)
+//    {
+//
+//        List <Integer> al = new ArrayList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//        System.out.println("After transversing");
+//        while(k.hasPrevious())
+//        {
+//            System.out.println(k.previous());
+//
+//        }
+//    }
+//}
+
+///Even and odd numbers through Iterator
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        ArrayList<Integer> al = new ArrayList<>();
+//        al.add(34);
+//        al.add(57);
+//       Iterator <Integer> k= al.iterator();
+//
+//       while(k.hasNext())
+//       {
+//           int i=k.next();
+//           if(i%2==0)
+//           {
+//               System.out.println("Even numbers "+i);
+//            }
+//           else
+//           {
+//               System.out.println("Odd numbers "+i);
+//           }
+//
+//        }
+//    }
+//}
+//
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        LinkedList<Integer> al = new LinkedList<>();
+//        al.add(34);
+//        al.add(57);
+//        Iterator <Integer> k= al.iterator();
+//
+//        while(k.hasNext())
+//        {
+//            int i=k.next();
+//            if(i%2==0)
+//            {
+//                System.out.println("Even numbers "+i);
+//            }
+//            else
+//            {
+//                System.out.println("Odd numbers "+i);
+//            }
+//
+//        }
+//    }
+//}
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        Vector<Integer> al = new Vector<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        Iterator <Integer> k= al.iterator();
+//
+//        while(k.hasNext())
+//        {
+//            int i=k.next();
+//            if(i%2==0)
+//            {
+//                System.out.println("Even numbers "+i);
+//            }
+//            else
+//            {
+//                System.out.println("Odd numbers "+i);
+//            }
+//
+//        }
+//    }
+//}
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        Stack<Integer> al = new Stack<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        Iterator <Integer> k= al.iterator();
+//
+//        while(k.hasNext())
+//        {
+//            int i=k.next();
+//            if(i%2==0)
+//            {
+//                System.out.println("Even numbers "+i);
+//            }
+//            else
+//            {
+//                System.out.println("Odd numbers "+i);
+//            }
+//
+//        }
+//    }
+//}
+
+//_________________________________________________________________________________________________________________________
+
+//ListIterator in front direction.
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        List <Integer> al = new ArrayList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//    }
+//}
+
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args) {
+//
+//        List <Integer> al = new LinkedList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//    }
+//}
+
+//___________________________________________________________________________________________________________________________
+
+//ListIterator in backward dircetion
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args)
+//    {
+//
+//        List <Integer> al = new LinkedList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//        System.out.println("After transversing");
+//        while(k.hasPrevious())
+//        {
+//            System.out.println(k.previous());
+//
+//        }
+//    }
+//}
+
+//import java.util.*;
+//
+//public class Enumer {
+//    public static void main(String[] args)
+//    {
+//
+//        List <Integer> al = new LinkedList<>();
+//        al.add(34);
+//        al.add(57);
+//        al.add(45);
+//        al.add(46);
+//        al.add(47);
+//        ListIterator <Integer> k= al.listIterator();
+//
+//        while(k.hasNext())
+//        {
+//            System.out.println(k.next());
+//
+//        }
+//        System.out.println("After transversing");
+//        while(k.hasPrevious())
+//        {
+//            System.out.println(k.previous());
+//            System.out.println(k.previousIndex());
+//
+//        }
+//    }
+//}
